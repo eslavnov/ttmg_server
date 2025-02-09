e44dd2b9982a1b17126d42dd2bb6805709696713
Evgeny Slavnov <eslavnov@gmail.com>
2025-02-09 16:43:57 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,9 @@ source venv/bin/activate
 # Install the requirements
 pip install -r requirements.txt
 
+# Create config
+if [ ! -f configuration.json ]; then
+  cp configuration_example.json configuration.json
+fi
+
 echo "Setup complete. Virtual environment created and dependencies installed."
